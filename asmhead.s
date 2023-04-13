9a69e0e7686b4f82f5cd103d44057eadabe1bab7
zm <missingurchin@gmail.com>
2023-04-13 16:28:07 +0800

    Add core which is split into 2 files
    one is asmhead.s
    another is bootpack.c

---
+++ asmhead.s
@@ -0,0 +1,22 @@
+CYLS  equ  0x0ff0
+LEDS  equ  0x0ff1
+VMODE equ  0x0ff2
+SCRNX equ  0x0ff4
+SCRNY equ  0x0ff6
+VRAM  equ  0x0ff8
+
+org 0xca00   ;core at ca00
+mov al,0x13
+mov ah,0x00
+int 0x10
+mov byte [VMODE],8
+mov word [SCRNX],320
+mov word [SCRNY],200
+mov dword [VRAM],0x000a0000
+
+mov ah,0x02
+int 0x16
+mov [LEDS],al
+fin:
+    hlt
+    jmp fin
