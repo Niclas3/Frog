1c9f6473a5feeffee14ecdb7101f9e8b90046665
zm <missingurchin@gmail.com>
2023-04-24 17:40:48 +0800

    Add draw box function and some issues
    1. `void HariMain(void)` this function must be first function in
       bootpack.c

--- bootpack.c
+++ bootpack.c
@@ -1,18 +1,120 @@
+// define
+#define COL8_000000 0
+#define COL8_FF0000 1
+#define COL8_00FF00 2
+#define COL8_FFFF00 3
+#define COL8_0000FF 4
+#define COL8_FF00FF 5
+#define COL8_00FFFF 6
+#define COL8_FFFFFF 7
+#define COL8_C6C6C6 8
+#define COL8_840000 9
+#define COL8_008400 10
+#define COL8_848400 11
+#define COL8_000084 12
+#define COL8_840084 13
+#define COL8_008484 14
+#define COL8_848484 15
+
+// Function from core.s
 void _io_hlt(void);
 /* void _print(char* msg, int len); */
 /* void _print(void); */
 void _write_mem8(int addr, int data);
 
-void HariMain(void) {
-  int i;
-  char *p = (char *)0xa0000;
-  // 0xa0000-0x0ffff
-  for (i = 0x00000; i <= 0x0ffff; i++) {
-    *(p + i) = i & 0x0f;
-    /* _write_mem8(i, i & 0x0f); */
-  }
+void _io_cli(void);
+void _io_out8(int port, int data);
+int _io_load_eflags(void);
+void _io_store_eflags(int eflags);
+//-------------------------------------
+// dec
+void boxfill8(unsigned char *vram,
+              int xsize,
+              unsigned char c,
+              int x0,
+              int y0,
+              int x1,
+              int y1);
+void set_palette(int start, int end, unsigned char *rgb);
+void init_palette(void);
+//-------------------------------------
 
-  for (;;) {
-    _io_hlt(); // execute _io_hlt in naskfunc.s
-  }
+// HariMain must at top of file
+void HariMain(void)
+{
+    int i;
+    char *p = (char *) 0xa0000;
+    /* init_palette(); */
+
+    // 0xa0000-0x0ffff
+    /* for (i = 0x00000; i <= 0x0ffff; i++) { */
+    /*     *(p + i) = i & 0x0f; */
+    /*     #<{(| _write_mem8(i, i & 0x0f); |)}># */
+    /* } */
+
+    boxfill8(p, 320, COL8_FFFFFF, 20, 20, 120, 120);
+    boxfill8(p, 320, COL8_FFFFFF, 70, 50, 170, 150);
+    boxfill8(p, 320, COL8_FFFF00, 120, 80, 220, 180);
+
+    for (;;) {
+        _io_hlt();  // execute _io_hlt in naskfunc.s
+    }
 }
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+    eflags = _io_load_eflags();
+    _io_cli();
+    _io_out8(0x03c8, start);
+    for (i = start; i <= end; i++) {
+        _io_out8(0x03c9, rgb[0] / 4);
+        _io_out8(0x03c9, rgb[1] / 4);
+        _io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    _io_store_eflags(eflags);
+    return;
+}
+
+void init_palette(void)
+{
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00,  // 00 black
+        0xff, 0x00, 0x00,  // 01 light red
+        0x00, 0xff, 0x00,  // 02 light green
+        0xff, 0xff, 0x00,  // 03 light yellow
+        0x00, 0x00, 0xff,  // 04 light blue
+        0xff, 0x00, 0xff,  // 05 light purple
+        0x00, 0xff, 0xff,  // 06 light light blue
+        0xff, 0xff, 0xff,  // 07 write
+        0xc6, 0xc6, 0xc6,  // 08 light gray
+        0x84, 0x00, 0x00,  // 09 dark red
+        0x00, 0x84, 0x00,  // 10 dark green
+        0x84, 0x84, 0x00,  // 11 dark yellow
+        0x00, 0x00, 0x84,  // 12 dark purple
+        0x00, 0x84, 0x84,  // 13 light dark blue
+        0x84, 0x84, 0x84,  // 14 dark gray
+    };
+    set_palette(0, 0x0f, table_rgb);
+    return;
+}
+
+void boxfill8(unsigned char *vram,
+              int xsize,
+              unsigned char c,
+              int x0,
+              int y0,
+              int x1,
+              int y1)
+{
+
+    int x, y;
+    for (y = y0; y <= y1; y++) {
+        for (x = x0; x <= x1; x++) {
+            vram[y * xsize + x] = c;
+        }
+    }
+    return;
+}
+
--- core.s
+++ core.s
@@ -1,26 +1,89 @@
 %include "./boot.inc"
 extern HariMain
+
 global _start
-global _write_mem8 ;_void _write_mem8(int addr, int data);
+
+global _write_mem8      ;_void _write_mem8(int addr, int data);
 global _io_hlt
-; [section start] vstart=0xd400
+global _io_cli,   _io_sti,  _io_stihlt
+global _io_in8,  _io_in16,  _io_in32
+global _io_out8, _io_out16, _io_out32
+global _io_load_eflags, _io_store_eflags
 
 SELECTOR_VGC   equ (0x0004<<3) + TI_GDT + RPL0
 _start:
     call HariMain
     jmp $
 
+;-----------------functions-----------------------
 _write_mem8:
     mov ecx,[esp+4]
     mov al,[esp+8]
     mov dx, SELECTOR_VGC
     mov edi,ecx
     mov gs, dx
-    ; mov byte [gs:0], al
     mov byte [gs:ecx], al
-    ; mov [ecx],al
     ret
 
 _io_hlt:    ; void io_hlt(void);
     hlt
+    ret
+
+_io_cli:    ; void io_cli(void)
+    cli     ; clear interrupt eflags
+    ret
+
+_io_sti:    ; void io_sti(void)
+    sti     ; set interrupt eflags
+    ret
+
+_io_stihlt: ; void io_stihlt(void)
+    sti
+    hlt
+    ret
+
+_io_in8:    ; void io_in8(int port)
+    mov edx,[esp+4]   ;port
+    mov eax,0
+    in  al,dx
+    ret
+
+_io_in16:    ; void io_in16(int port)
+    mov edx,[esp+4]   ;port
+    mov eax,0
+    in  al,dx
+    ret
+
+_io_in32:    ; void io_in32(int port)
+    mov edx,[esp+4]   ;port
+    in eax,dx
+    ret
+
+_io_out8:    ; void io_out8(int port, int data)
+    mov edx,[esp+4]  ; port
+    mov al,[esp+8]   ; data
+    out dx, al
+    ret
+
+_io_out16:    ; void io_out16(int port, int data)
+    mov edx,[esp+4]  ; port
+    mov al,[esp+8]   ; data
+    out dx, ax
+    ret
+
+_io_out32:    ; void io_out32(int port, int data)
+    mov edx,[esp+4]  ; port
+    mov eax,[esp+8]   ; data
+    out dx, eax
+    ret
+
+_io_load_eflags: ; int io_load_eflags(void)
+    pushfd       ; push flags double-word
+    pop eax
+    ret
+
+_io_store_eflags: ; void io_store_eflags(int eflags)
+    mov eax, [esp+4]
+    push eax
+    popfd      ; pop flags double-word
     ret
