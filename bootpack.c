cf0b77e3ee9266990f89991ca656c7d2555aaff8
zm <missingurchin@gmail.com>
2023-04-25 14:13:55 +0800

    Draw a desktop

--- bootpack.c
+++ bootpack.c
@@ -42,19 +42,39 @@ void init_palette(void);
 // HariMain must at top of file
 void HariMain(void)
 {
-    int i;
-    char *p = (char *) 0xa0000;
     /* init_palette(); */
 
     // 0xa0000-0x0ffff
-    /* for (i = 0x00000; i <= 0x0ffff; i++) { */
+    /* for (int i = 0x00000; i <= 0x0ffff; i++) { */
     /*     *(p + i) = i & 0x0f; */
     /*     #<{(| _write_mem8(i, i & 0x0f); |)}># */
     /* } */
 
-    boxfill8(p, 320, COL8_FFFFFF, 20, 20, 120, 120);
-    boxfill8(p, 320, COL8_FFFFFF, 70, 50, 170, 150);
-    boxfill8(p, 320, COL8_FFFF00, 120, 80, 220, 180);
+    char *vram;
+    int xsize, ysize;
+
+    /* init_palette(); */
+    vram = (char *) 0xa0000;
+    xsize = 320;
+    ysize = 200;
+
+    boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+    boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+    boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+    boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
+
+    boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+    boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+    boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+    boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+    boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+    boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+    boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+    boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+
 
     for (;;) {
         _io_hlt();  // execute _io_hlt in naskfunc.s
