0ad815879a90a636033bc10d139c46590bbe24d0
zm <missingurchin@gmail.com>
2023-07-10 22:22:30 +0800

    Init interrupt and try to control CODE_SEGMENT
    1. Create interrupt descriptor table (IDT).
    2. Set code descriptor at runtime.

--- booter/naskfunc.s
+++ booter/naskfunc.s
@@ -1,7 +1,8 @@
 %include "../header/boot.inc"
-section loader vstart=LOADER_BASE_ADDR ;0xc400
+; section loader vstart=LOADER_BASE_ADDR ;0xc400
 
 LOADER_STACK_TOP  equ  LOADER_BASE_ADDR
+org 0xc400
 jmp loader_start
 
 ;; memory descriptor
@@ -11,6 +12,7 @@ jmp loader_start
 ;; db 1 bytes
 ;; dw 2 bytes
 ;; dd 4 bytes
+; [section .gdt]
 GDT_BASE   dd  0x00000000  ; low
            dd  0x00000000  ; high
 
@@ -25,13 +27,13 @@ CODE_DESC  dd  0x0000FFFF
 ; ;----new----------------------------------------
 ; 0000 0000 1000 0000 0000 0000 ; DESC_G_4K
 ; 0000 0000 0100 0000 0000 0000 ; DESC_D_32
-; 0000 0000 0000 0000 0000 0000 ; L
+; 0000 0000 0000 0000 1000 0000 ; DESC_P
 ; 0000 0000 0000 0000 0000 0000 ; DESC_AVL
 ; 0000 0000 0000 1111 0000 0000 ; DESC_LIMIT_CODE2
-; 0000 0000 0000 0000 1000 0000 ; DESC_P
 ; 0000 0000 0000 0000 0000 0000 ; DESC_DPL_0
 ; 0000 0000 0000 0000 0001 0000 ; DESC_S_CODE  ;DT1
 ; 0000 0000 0000 0000 0000 1000 ; DESC_TYPE_CODE ; TYPE ;X C R A
+; 0000 0000 0000 0000 0000 0000 ; L
 
 ; 0000 0000 1100 1111 1001 1000
 ;           C    F    9    8
@@ -80,6 +82,27 @@ SELECTOR_VGC   equ (0x0004<<3) + TI_GDT + RPL0
 gdt_ptr dw GDT_LIMIT
         dd GDT_BASE
 
+;; IDT
+; [section .idt]
+; [bits 32]
+IDT_BASE:
+%rep 255
+         dw  (Putchar & 0xFFFF)
+         dw  SELECTOR_CODE
+         dw  INTR_GATE_HIGH2
+         dw  ((Putchar >> 16) & 0xFFFF)
+%endrep
+
+IDT_SIZE equ $$-IDT_BASE
+IDT_LIMIT equ IDT_SIZE - 1
+
+idt_ptr  dw IDT_LIMIT
+         dd IDT_BASE
+
+;; END IDT
+
+; [section .s16]
+; [bits 16]
 loadermsg db '2 loader in real.'
           db 0
 
@@ -90,6 +113,7 @@ mov word [SCRNX],320
 mov word [SCRNY],200
 mov dword [VRAM],0x000a0000
 ;----------------------------print message -------------------------------
+print_begin:
     mov ax, loadermsg
     mov si, ax
 _print_begin:
@@ -102,15 +126,24 @@ _print_begin:
     mov bx, 15
     int 10h
     jmp _print_begin
-;---------------
+
 ;--reset black screen---------------------------
 rst_b_scr:
-    mov al,0x13
-    mov ah,0x00
-    int 0x10
+      nop
+;     mov al,0x13
+;     mov ah,0x00
+;     int 0x10
 ;----------------------------------------------------
 
-;-----------------------------------------------------
+; Init 32 bits code description
+    xor eax, eax
+    mov ax, cs
+    shl eax, 4
+    add eax, LABEL_SEG_CODE32
+    mov word [CODE_DESC+ 2], ax
+    shr eax, 16
+    mov byte [CODE_DESC+ 4], al
+    mov byte [CODE_DESC+ 7], ah
 ; A20 open
 
 in al, 0x92
@@ -125,10 +158,18 @@ mov eax, cr0
 or  eax, 0x00000001
 mov cr0, eax
 
-jmp dword SELECTOR_CODE:p_mode_start ; reflash code-flow?
+;--------------- init idt ----------------------
+init_idt:
+cli
+lidt [idt_ptr]
 
-[bits 32]
-p_mode_start:
+;-----------------------------------------------------
+; jmp dword SELECTOR_CODE: LABEL_SEG_CODE32; reflash code-flow?
+jmp dword SELECTOR_CODE: 0; reflash code-flow?
+
+section .s32
+bits 32
+LABEL_SEG_CODE32:
     mov ax, SELECTOR_DATA
     mov ds, ax
     mov es, ax
@@ -138,6 +179,65 @@ p_mode_start:
     mov ax, SELECTOR_VIDEO
     mov gs, ax
 
-    mov byte [gs:160], 'X'
-
+    mov byte [gs:160], 'Z'
+    ; call Init8259A
+    int 80h
     jmp dword SELECTOR_CODE:0xd800
+
+_Putchar:
+Putchar equ _Putchar - $$
+    mov ah, 0ch
+    mov al, `!`
+    ; mov byte [gs:160], ax
+    mov word [gs:160], ax
+    jmp $
+    iretd
+
+Init8259A:
+    mov al, 011h
+    out 020h, al
+    call io_delay
+
+    out 0A0h, al
+    call io_delay
+
+    mov al, 020h
+    out 021h, al
+    call io_delay
+
+    mov al, 028h
+    out 0A1h, al
+    call io_delay
+
+    mov al, 004h
+    out 021h, al
+    call io_delay
+
+    mov al, 002h
+    out 0A1h, al
+    call io_delay
+
+    mov al, 001h
+    out 021h, al
+    call io_delay
+
+    out 0A1h, al
+    call io_delay
+
+    mov al, 11111110b
+    out 021h, al
+    call io_delay
+
+
+    mov al, 11111111b
+    out 0A1h, al
+    call io_delay
+    ret
+
+io_delay:
+    nop
+    nop
+    nop
+    nop
+    ret
+
