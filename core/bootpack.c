dd836f7ebe9be626d235271a7bdff650582e539c
zm <missingurchin@gmail.com>
2023-07-25 21:24:21 +0800

    Test C-version descriptor creator

--- core/bootpack.c
+++ core/bootpack.c
@@ -1,5 +1,6 @@
 #include "include/graphic.h"
 #include "include/bootpack.h"
+#include "include/descriptor.h"
 
 //-------------------------------------
 typedef struct B_info {
@@ -14,24 +15,46 @@ typedef struct B_info {
 typedef struct Color {
     unsigned char color_id;
 } COLOR;
-// HariMain must at top of file
 
+void test_function();
+// HariMain must at top of file
 void HariMain(void)
 {
     char font_A[16] = {0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
                        0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00};
 
     char *hankaku = (char *) FONT_HANKAKU; // size 4096
+                                           
+    /* Gate_Descriptor *test_gate_desc = 0x7c00; */
+    Segment_Descriptor *code_descriptor = 0x7c00;
+    create_descriptor(code_descriptor,
+                      0x0,
+                      0xffffffff,
+                      DESC_P_1|DESC_DPL_0|
+                      DESC_S_DATA|DESC_TYPE_CODEX,
+                      DESC_G_4K|DESC_D_32|
+                      DESC_L_32bit|DESC_AVL);
+
+    /* Selector selector_1 = create_selector(1,0,0); */
+
+    /* create_gate(test_gate_desc, */
+    /*             selector_1, */
+    /*             test_function, */
+    /*             0, */
+    /*             0); */
 
     init_palette();
+    half_byte d;
+    d.value = 0xff;
+
     unsigned char *vram;
     int xsize, ysize;
     vram = (unsigned char *) 0xa0000;
     xsize = 320;
     ysize = 200;
 
-    char *mcursor =(char*) 0x7c00; 
-    char *mcursor1 =(char*) 0x7c01;
+    /* char *mcursor =(char*) 0x7c00;  */
+    /* char *mcursor1 =(char*) 0x7c01; */
 
     COLOR c = {.color_id = COL8_008484};
 
@@ -44,12 +67,12 @@ void HariMain(void)
     int py0 = 50;
     int px0 = 50;
 
-    draw_cursor8(mcursor, COL8_848484);
-    draw_cursor8(mcursor1, COL8_008484);
-    int mx = 70;
-    int my = 50;
-    putblock8_8((char *)vram, xsize, 16, 16, mx, my, mcursor, 16);
-    putblock8_8((char *)vram, xsize, 16, 16, mx+40, my+20, mcursor1, 16);
+    /* draw_cursor8(mcursor, COL8_848484); */
+    /* draw_cursor8(mcursor1, COL8_008484); */
+    /* int mx = 70; */
+    /* int my = 50; */
+    /* putblock8_8((char *)vram, xsize, 16, 16, mx, my, mcursor, 16); */
+    /* putblock8_8((char *)vram, xsize, 16, 16, mx+40, my+20, mcursor1, 16); */
 
 
     /* char s[16] = {0}; */
@@ -65,5 +88,9 @@ void HariMain(void)
     for (;;) {
         _io_hlt();  // execute _io_hlt in naskfunc.s
     }
+}
+
+void test_function(){
+    putfonts8_asc((char *)0xa0000, 320, 8, 8, COL8_0000FF, "Niclas 123");
 }
 
