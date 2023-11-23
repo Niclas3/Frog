551e9d78ab178871b8311fedb7a174ef5f8fb0d9
zm <missingurchin@gmail.com>
2023-11-23 21:59:22 +0800

    Add Macro MAX() MIN()

--- core/include/math.h
+++ core/include/math.h
@@ -3,4 +3,14 @@
 
 #define DIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
 
+#define MAX(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define MIN(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
 #endif
