580f2885002798bfc15537371505f05024d46a8e
zm <missingurchin@gmail.com>
2023-12-12 11:38:05 +0800

    Fix Export vsprintf()

--- core/include/stdio.h
+++ core/include/stdio.h
@@ -1,7 +1,9 @@
 #ifndef __LIB_STDIO_H
 #define __LIB_STDIO_H
 #include <ostype.h>
+#include <stdarg.h>
 
+uint_32 vsprintf(char *str, const char *fmt, va_list ap);
 uint_32 sprintf(char* str, const char *fmt, ...);
 uint_32 printf(const char *fmt, ...);
 
