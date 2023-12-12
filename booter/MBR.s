e4c962347848c3ba31628f846b40f36a6cdc6e08
zm <missingurchin@gmail.com>
2023-12-12 11:59:48 +0800

    Refine MBR 1 level loader code

--- booter/MBR.s
+++ booter/MBR.s
@@ -1,41 +1,10 @@
 org 0x7c00
 %include "boot.inc"
 
-xor ax, ax             ; Ensure data & extra segments are 0 to start, can help
-mov es, ax             ; with booting on hardware
-mov ds, ax
-;; Read 
-mov al, 15          ; 8 - 1 for boot block - bootsector & 8 for super block
-mov bl, al
-dec bl              ; BX = # of sectors to read - 1
-mov di, 0xC400       ; Read sectors to this address
-
-mov dx, 1F6h        ; Head / drive port, flags
-mov al, 0xE0        ; 0b0111 0000, bits: 0-3 = LBA bits 24-27, 4 = drive, 5 = always set, 6 = set for LBA, 7 = always set
-out dx, al
-
-mov dx, 1F2h        ; Sector count port
-mov al, 15          ; # of sectors to read
-out dx, al
-
-inc dx              ; 1F3h, sector # port / LBA low
-mov al, 2           ; LBA 1 = 2nd disk sector (0-based), bits 0-7 of LBA
-out dx, al
-
-inc dx              ; 1F4h, Cylinder low / LBA mid
-xor ax, ax          ; bits 8-15 of LBA
-out dx, al
-
-inc dx              ; 1F5h, Cylinder high / LBA high
-; xor ax, ax        ; bits 16-23 of LBA
-out dx, al
-
-inc dx
-inc dx              ; 1F7h, Command port
-mov al, 20h         ; Read with retry
-out dx, al
-
-call load_sector_loop
+push 13                  ; number of sectors to read for 6.5k
+push LOADER_BASE_ADDR    ; target memory address
+push 2                   ; read start lba address
+call read_hard_disk_qemu_bit16
 
 jmp LOADER_BASE_ADDR
 
@@ -66,36 +35,76 @@ ide_identify:
         jnz .go_on_read
         ret
 
-;------------------------------------------------------------
+;===============================================================================
+; void read_hard_disk_qemu (LBA_sector_number, base_address, count_sector)
 ; Read n sector from hard disk 
-;; di : address to read into.
-;; 
-;; Poll status port after reading 1 sector
-load_sector_loop:
-    in al, dx           ; Status register (reading port 1F7h)
-    test al, 8          ; Sector buffer requires servicing 
-    je load_sector_loop ; Keep trying until sector buffer is ready
+;; ebp +2 ---> LBA sector number
+;; ebp +4 ---> base address
+;; ebp +6 ---> count read-in sector number
+;===============================================================================
+read_hard_disk_qemu_bit16:
+    mov ebp, esp
+    ;; Read
+    mov edi, [ebp+4]         ; Read sectors to this address
+    mov ebx, [ebp+6]         ; count of sectors to read 
 
-    mov cx, 256         ; # of words to read for 1 sector
-    mov dx, 1F0h        ; Data port, reading 
-    rep insw            ; Read bytes from DX port # into DI, CX # of times
+    mov dx, 1F6h        ; Head / drive port, flags
+    mov al, 0xE0        ; 0b0111 0000,
+                        ; bits: 0-3 = LBA bits 24-27, 
+                        ;         4 = drive, 
+                        ;         5 = always set, 
+                        ;         6 = set for LBA, 
+                        ;         7 = always set
+    out dx, al
 
-    ;; 400ns delay - Read alternate status register
-    mov dx, 3F6h
-    in al, dx
-    in al, dx
-    in al, dx
-    in al, dx
+;;  set sector count for reading
+    mov dx, 1F2h        ; Sector count port
+    mov al, [ebp+6]         ; of sectors to read
+    out dx, al
 
-    cmp bl, 0           ; Sector # to still read
-    je .return
+    inc dx              ; 1F3h, sector # port / LBA low
+    mov eax, [ebp+2]     ; LBA 1, bits 0-7 of LBA
+    out dx, al
 
-    dec bl
-    mov dx, 1F7h
-    jmp load_sector_loop
+    inc dx              ; 1F4h, Cylinder low / LBA mid
+    mov cl, 8
+    shr eax, cl
+    out dx, al
 
-.return:
-ret
+    inc dx              ; 1F5h, Cylinder high / LBA high
+    shr eax, cl         ; bits 16-23 of LBA
+    out dx, al
+
+    inc dx
+    inc dx              ; 1F7h, Command port
+    mov al, 20h         ; Read with retry
+    out dx, al
+
+    load_sector_loop:
+        in al, dx           ; Status register (reading port 1F7h)
+        and al, 0x88
+        cmp al, 0x08          ; Sector buffer requires servicing 
+        jnz load_sector_loop ; Keep trying until sector buffer is ready
+
+        mov cx, 256         ; 2bytes 1 time. of words to read for 1 sector
+        mov dx, 1F0h        ; Data port, reading 
+        rep insw            ; Read bytes from EDX port # into DI, CX # of times
+
+        ;; 400ns delay - Read alternate status register
+        mov dx, 3F6h
+        in al, dx
+        in al, dx
+        in al, dx
+        in al, dx
+
+        dec bl
+        cmp bl, 0           ; Sector # to still read
+        je .return
+
+        mov dx, 1F7h
+        jmp load_sector_loop
+    .return:
+    ret
 
 msg:
     DB 0x0a, 0x0a    ; Two line breaks
