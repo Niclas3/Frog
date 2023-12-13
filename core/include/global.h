02ea29953cd449b67847fac353c123ccbe1386a9
zm <missingurchin@gmail.com>
2023-12-13 14:22:17 +0800

    Add global fixed address
    1.A pointer to memory infomation struct.
    2.A pointer to VBE mode struct and info struct.

--- core/include/global.h
+++ core/include/global.h
@@ -16,4 +16,10 @@ typedef struct Color {
 
 extern BOOTINFO info;
 
+#define VBE_MODE_INFO_POINTER   0x0ffc
+#define VBE_INFO_POINTER        0x1000
+
+#define MMAP_INFO_POINTER       0x0ff8
+#define MMAP_INFO_COUNT_POINTER 0x0ff4 //; 2 bytes
+
 #endif
