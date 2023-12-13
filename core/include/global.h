45c3438472bf8e18af34a06c1d97da2ff3b0d736
zm <missingurchin@gmail.com>
2023-12-13 15:30:44 +0800

    Remove useless code

--- core/include/global.h
+++ core/include/global.h
@@ -1,20 +1,6 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
-typedef struct B_info {
-    char cyls;
-    char leds;
-    char vmode;
-    char reserve;
-    short scrnx, scrny;
-    unsigned char *vram;
-} BOOTINFO;
-
-typedef struct Color {
-    unsigned char color_id;
-} COLOR;
-
-extern BOOTINFO info;
 
 #define VBE_MODE_INFO_POINTER   0x0ffc
 #define VBE_INFO_POINTER        0x1000
