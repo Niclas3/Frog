ad3beb1226807555db5b577841d1b6b0e6331f94
zm <missingurchin@gmail.com>
2023-12-14 00:16:03 +0800

    Add draw_2d_gfx_8bit_font() for displaying 8 bits font at VBE mode

--- core/gui/2d_graphics.c
+++ core/gui/2d_graphics.c
@@ -1,5 +1,4 @@
 #include <sys/2d_graphics.h>
-#include <sys/FSK_Color.h>
 
 #include <const.h>
 #include <global.h>
@@ -58,6 +57,35 @@ void twoD_graphics_init(void)
             break;
         }
     }
+}
+
+static void draw_2d_gfx_8bit_font(int font_size,
+                                  int_16 x,
+                                  int_16 y,
+                                  uint_32 color,
+                                  char *font)
+{
+    char d;
+    for (int i = 0; i < 16; i++) {
+        // go though all bits each line has 8 bits
+        d = font[i];
+        int_8 bit_idx = 0;
+        while (bit_idx < 8) {
+            if (d & 0x01) {
+                draw_pixel(x + (7 - bit_idx), (y + i), color);
+            }
+            bit_idx++;
+            d >>= 1;
+        }
+    }
+    return;
+}
+
+void draw_2d_gfx_asc_char(int font_size, int x, int y, uint_32 color, char num)
+{
+    /* char *hankaku = (char *) FONT_HANKAKU; */
+    char *hankaku = (char *) 0xc000d800;
+    draw_2d_gfx_8bit_font(font_size, x, y, color, hankaku + (num * 16));
 }
 
 // Convert given 32bit 888ARGB color to set bpp value
@@ -119,24 +147,33 @@ void draw_pixel(uint_16 X, uint_16 Y, uint_32 color)
 //   Adapted from Wikipedia page on Bresenham's line algorithm
 void draw_line(Point start, Point end, uint_32 color)
 {
-    int_16 deltaX = ABS((end.X - start.X));    // Delta X, change in X values, positive absolute value
-    int_16 deltaY = -ABS((end.Y - start.Y));   // Delta Y, change in Y values, negative absolute value
-    int_16 signX = (start.X < end.X) ? 1 : -1; // Sign of X direction, moving right (positive) or left (negative)
-    int_16 signY = (start.Y < end.Y) ? 1 : -1; // Sign of Y direction, moving down (positive) or up (negative)
+    int_16 deltaX =
+        ABS((end.X -
+             start.X));  // Delta X, change in X values, positive absolute value
+    int_16 deltaY = -ABS(
+        (end.Y -
+         start.Y));  // Delta Y, change in Y values, negative absolute value
+    int_16 signX =
+        (start.X < end.X) ? 1 : -1;  // Sign of X direction, moving right
+                                     // (positive) or left (negative)
+    int_16 signY =
+        (start.Y < end.Y) ? 1 : -1;  // Sign of Y direction, moving down
+                                     // (positive) or up (negative)
     int_16 error = deltaX + deltaY;
     int_16 errorX2;
 
     while (1) {
         draw_pixel(start.X, start.Y, color);
-        if (start.X == end.X && start.Y == end.Y) break;
+        if (start.X == end.X && start.Y == end.Y)
+            break;
 
         errorX2 = error * 2;
         if (errorX2 >= deltaY) {
-            error   += deltaY;
+            error += deltaY;
             start.X += signX;
         }
         if (errorX2 <= deltaX) {
-            error   += deltaX;
+            error += deltaX;
             start.Y += signY;
         }
     }
@@ -155,7 +192,7 @@ void draw_triangle(Point vertex0, Point vertex1, Point vertex2, uint_32 color)
 void draw_rect(Point top_left, Point bottom_right, uint_32 color)
 {
     Point temp = bottom_right;
-    
+
     // Draw 4 lines, 2 horizontal parallel sides, 2 vertical parallel sides
     // Top of rectangle
     temp.Y = top_left.Y;
@@ -178,7 +215,7 @@ void draw_polygon(Point vertex_array[], uint_8 num_vertices, uint_32 color)
 {
     // Draw lines up to last line
     for (uint_8 i = 0; i < num_vertices - 1; i++)
-        draw_line(vertex_array[i], vertex_array[i+1], color);
+        draw_line(vertex_array[i], vertex_array[i + 1], color);
 
     // Draw last line
     draw_line(vertex_array[num_vertices - 1], vertex_array[0], color);
@@ -204,10 +241,11 @@ void draw_circle(Point center, uint_16 radius, uint_32 color)
 
     while (x < y) {
         x++;
-        if (p < 0) p += 2*x + 1;
+        if (p < 0)
+            p += 2 * x + 1;
         else {
             y--;
-            p += 2*(x-y) + 1;
+            p += 2 * (x - y) + 1;
         }
 
         // Draw next set of 8 octant points
@@ -223,9 +261,11 @@ void draw_circle(Point center, uint_16 radius, uint_32 color)
 }
 
 // Draw an ellipse
-// TODO: Put this back in later, using non-floating point values/ROUND if possible
+// TODO: Put this back in later, using non-floating point values/ROUND if
+// possible
 //  Adapted from "Computer Graphics Principles and Practice in C 2nd Edition"
-//void draw_ellipse(Point center, uint_16 radiusX, uint_16 radiusY, uint_32 color)
+// void draw_ellipse(Point center, uint_16 radiusX, uint_16 radiusY, uint_32
+// color)
 //{
 //    int32_t rx2 = radiusX * radiusX;
 //    int32_t ry2 = radiusY * radiusY;
@@ -282,19 +322,23 @@ void draw_circle(Point center, uint_16 radius, uint_32 color)
 //}
 
 // Fill an area with a solid color
-void boundary_fill(uint_16 X, uint_16 Y, uint_32 fill_color, uint_32 boundary_color)
+void boundary_fill(uint_16 X,
+                   uint_16 Y,
+                   uint_32 fill_color,
+                   uint_32 boundary_color)
 {
     // Recursive - may use a lot of stack space
-    uint_8 *framebuffer    = (uint_8 *)g_gfx_mode->physical_base_pointer; 
-    uint_8 bytes_per_pixel = (g_gfx_mode->bits_per_pixel+1) / 8;             // Get # of bytes per pixel, add 1 to fix 15bpp modes
+    uint_8 *framebuffer = (uint_8 *) g_gfx_mode->physical_base_pointer;
+    uint_8 bytes_per_pixel =
+        (g_gfx_mode->bits_per_pixel + 1) /
+        8;  // Get # of bytes per pixel, add 1 to fix 15bpp modes
     uint_8 draw = 0;
 
     framebuffer += (Y * g_gfx_mode->x_resolution + X) * bytes_per_pixel;
 
     for (uint_8 temp = 0; temp < bytes_per_pixel; temp++) {
-        if ((framebuffer[temp] != (uint_8)(fill_color >> (temp * 8))) &&
-            (framebuffer[temp] != (uint_8)(boundary_color >> (temp * 8)))) {
-
+        if ((framebuffer[temp] != (uint_8) (fill_color >> (temp * 8))) &&
+            (framebuffer[temp] != (uint_8) (boundary_color >> (temp * 8)))) {
             draw = 1;
             break;
         }
@@ -302,7 +346,7 @@ void boundary_fill(uint_16 X, uint_16 Y, uint_32 fill_color, uint_32 boundary_co
 
     if (draw) {
         for (uint_8 temp = 0; temp < bytes_per_pixel; temp++)
-            framebuffer[temp] = (uint_8)(fill_color >> temp * 8);
+            framebuffer[temp] = (uint_8) (fill_color >> temp * 8);
 
         // Check 4 pixels around current pixel
         boundary_fill(X + 1, Y, fill_color, boundary_color);
@@ -310,7 +354,7 @@ void boundary_fill(uint_16 X, uint_16 Y, uint_32 fill_color, uint_32 boundary_co
         boundary_fill(X, Y + 1, fill_color, boundary_color);
         boundary_fill(X, Y - 1, fill_color, boundary_color);
     }
-} 
+}
 
 
 // Fill triangle with a solid color
@@ -325,7 +369,7 @@ void fill_triangle_solid(Point p0, Point p1, Point p2, uint_32 color)
     // First draw triangles sides (boundaries)
     draw_triangle(p0, p1, p2, color - 1);
 
-    // Then fill in boundaries 
+    // Then fill in boundaries
     boundary_fill(temp.X, temp.Y, color, color - 1);
 
     // Then redraw boundaries to correct color
@@ -335,25 +379,28 @@ void fill_triangle_solid(Point p0, Point p1, Point p2, uint_32 color)
 // Fill rectangle with a solid color
 void fill_rect_solid(Point top_left, Point bottom_right, uint_32 color)
 {
-   // Brute force method 
-   for (uint_16 y = top_left.Y; y < bottom_right.Y; y++)
-       for (uint_16 x = top_left.X; x < bottom_right.X; x++)
-           draw_pixel(x, y, color);
+    // Brute force method
+    for (uint_16 y = top_left.Y; y < bottom_right.Y; y++)
+        for (uint_16 x = top_left.X; x < bottom_right.X; x++)
+            draw_pixel(x, y, color);
 }
 
 // Fill a polygon with a solid color
-void fill_polygon_solid(Point vertex_array[], uint_8 num_vertices, uint_32 color)
+void fill_polygon_solid(Point vertex_array[],
+                        uint_8 num_vertices,
+                        uint_32 color)
 {
     Point temp;
 
-    // Assuming this works in general, get center (Centroid) of a triangle in the polygon
+    // Assuming this works in general, get center (Centroid) of a triangle in
+    // the polygon
     temp.X = (vertex_array[0].X + vertex_array[1].X + vertex_array[2].X) / 3;
     temp.Y = (vertex_array[0].Y + vertex_array[1].Y + vertex_array[2].Y) / 3;
 
     // First draw polygon sides (boundaries)
     draw_polygon(vertex_array, num_vertices, color - 1);
 
-    // Then fill in boundaries 
+    // Then fill in boundaries
     boundary_fill(temp.X, temp.Y, color, color - 1);
 
     // Then redraw boundaries to correct color
@@ -366,13 +413,15 @@ void fill_circle_solid(Point center, uint_16 radius, uint_32 color)
     // Brute force method
     for (int_16 y = -radius; y <= radius; y++)
         for (int_16 x = -radius; x <= radius; x++)
-            if (x*x + y*y < radius*radius - radius)
+            if (x * x + y * y < radius * radius - radius)
                 draw_pixel(center.X + x, center.Y + y, color);
 }
 
 // Fill an ellipse with a solid color
-// TODO: Put this back in later, using non-floating point values/ROUND if possible
-//void fill_ellipse_solid(Point center, uint_16 radiusX, uint_16 radiusY, uint_32 color)
+// TODO: Put this back in later, using non-floating point values/ROUND if
+// possible
+// void fill_ellipse_solid(Point center, uint_16 radiusX, uint_16 radiusY,
+// uint_32 color)
 //{
 //    // First draw boundaries a different color
 //    draw_ellipse(center, radiusX, radiusY, color - 1);
--- core/include/sys/2d_graphics.h
+++ core/include/sys/2d_graphics.h
@@ -1,64 +1,68 @@
 #pragma onces
 
 #include <ostype.h>
+#include <sys/FSK_Color.h>
 
-//VBE infomation 
+// VBE infomation
 typedef struct vbe_info_structure {
-	char signature[4];	// must be "VESA" to indicate valid VBE support
-	uint_16 version;			// VBE version; high byte is major version, low byte is minor version
-	uint_32 oem_str_pointer;			// segment:offset pointer to OEM
-	uint_32 capabilities;		// bitfield that describes card capabilities
-	uint_32 video_modes_pointer;		// segment:offset pointer to list of supported video modes
-	uint_16 video_memory;		// amount of video memory in 64KB blocks
-	uint_16 software_rev;		// software revision
-	uint_32 vendor_pointer;			// segment:offset to card vendor string
-	uint_32 product_name_pointer;		// segment:offset to card model name
-	uint_32 product_rev_pointer;		// segment:offset pointer to product revision
-	char reserved[222];		// reserved for future expansion
-	char oem_data[256];		// OEM BIOSes store their strings in this area
-} __attribute__ ((packed)) vbe_info_t;
+    char signature[4];  // must be "VESA" to indicate valid VBE support
+    uint_16 version;    // VBE version; high byte is major version, low byte is
+                        // minor version
+    uint_32 oem_str_pointer;      // segment:offset pointer to OEM
+    uint_32 capabilities;         // bitfield that describes card capabilities
+    uint_32 video_modes_pointer;  // segment:offset pointer to list of supported
+                                  // video modes
+    uint_16 video_memory;         // amount of video memory in 64KB blocks
+    uint_16 software_rev;         // software revision
+    uint_32 vendor_pointer;       // segment:offset to card vendor string
+    uint_32 product_name_pointer;  // segment:offset to card model name
+    uint_32 product_rev_pointer;   // segment:offset pointer to product revision
+    char reserved[222];            // reserved for future expansion
+    char oem_data[256];  // OEM BIOSes store their strings in this area
+} __attribute__((packed)) vbe_info_t;
 
 // VBE Mode info block - holds current graphics mode values
 typedef struct {
     // Mandatory info for all VBE revisions
-	uint_16 mode_attributes;
-	uint_8 window_a_attributes;
-	uint_8 window_b_attributes;
-	uint_16 window_granularity;
-	uint_16 window_size;
-	uint_16 window_a_segment;
-	uint_16 window_b_segment;
-	uint_32 window_function_pointer;
-	uint_16 bytes_per_scanline;
+    uint_16 mode_attributes;
+    uint_8 window_a_attributes;
+    uint_8 window_b_attributes;
+    uint_16 window_granularity;
+    uint_16 window_size;
+    uint_16 window_a_segment;
+    uint_16 window_b_segment;
+    uint_32 window_function_pointer;
+    uint_16 bytes_per_scanline;
 
     // Mandatory info for VBE 1.2 and above
-	uint_16 x_resolution;
-	uint_16 y_resolution;
-	uint_8 x_charsize;
-	uint_8 y_charsize;
-	uint_8 number_of_planes;
-	uint_8 bits_per_pixel;
-	uint_8 number_of_banks;
-	uint_8 memory_model;
-	uint_8 bank_size;
-	uint_8 number_of_image_pages;
-	uint_8 reserved1;
+    uint_16 x_resolution;
+    uint_16 y_resolution;
+    uint_8 x_charsize;
+    uint_8 y_charsize;
+    uint_8 number_of_planes;
+    uint_8 bits_per_pixel;
+    uint_8 number_of_banks;
+    uint_8 memory_model;
+    uint_8 bank_size;
+    uint_8 number_of_image_pages;
+    uint_8 reserved1;
 
     // Direct color fields (required for direct/6 and YUV/7 memory models)
-	uint_8 red_mask_size;
-	uint_8 red_field_position;
-	uint_8 green_mask_size;
-	uint_8 green_field_position;
-	uint_8 blue_mask_size;
-	uint_8 blue_field_position;
-	uint_8 reserved_mask_size;
-	uint_8 reserved_field_position;
-	uint_8 direct_color_mode_info;
+    uint_8 red_mask_size;
+    uint_8 red_field_position;
+    uint_8 green_mask_size;
+    uint_8 green_field_position;
+    uint_8 blue_mask_size;
+    uint_8 blue_field_position;
+    uint_8 reserved_mask_size;
+    uint_8 reserved_field_position;
+    uint_8 direct_color_mode_info;
 
     // Mandatory info for VBE 2.0 and above
-	uint_32 physical_base_pointer;         // Physical address for flat memory frame buffer
-	uint_32 reserved2;                     // off_screen_mem_off
-	uint_16 reserved3;                     // off_screen_mem_size
+    uint_32
+        physical_base_pointer;  // Physical address for flat memory frame buffer
+    uint_32 reserved2;          // off_screen_mem_off
+    uint_16 reserved3;          // off_screen_mem_size
 
     // Mandatory info for VBE 3.0 and above
     uint_16 linear_bytes_per_scanline;
@@ -74,9 +78,9 @@ typedef struct {
     uint_8 linear_reserved_field_position;
     uint_32 max_pixel_clock;
 
-    uint_8 reserved4[190];              // Remainder of mode info block
+    uint_8 reserved4[190];  // Remainder of mode info block
 
-} __attribute__ ((packed)) vbe_mode_info_t;
+} __attribute__((packed)) vbe_mode_info_t;
 
 typedef enum {
     BOOT_VBE_MODE,
@@ -85,18 +89,20 @@ typedef enum {
     BOOT_UNKNOW
 } BOOT_GFX_MODE_t;
 
-// global variable for 2d graphics 
+// global variable for 2d graphics
 extern vbe_mode_info_t *g_gfx_mode;
 
-typedef struct{
+typedef struct {
     uint_32 X;
     uint_32 Y;
 } Point;
 
 BOOT_GFX_MODE_t boot_graphics_mode(void);
 void twoD_graphics_init(void);
-//for test
+// for test
 void clear_screen(uint_32 color);
 void draw_pixel(uint_16 X, uint_16 Y, uint_32 color);
 void fill_rect_solid(Point top_left, Point bottom_right, uint_32 color);
+
+void draw_2d_gfx_asc_char(int font_size, int x, int y, uint_32 color, char num);
 uint_32 convert_color(const uint_32 color);
