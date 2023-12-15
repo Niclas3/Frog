6584b6a700ff6eaf5d2cb5c55d8c3612e99b5ada
zm <missingurchin@gmail.com>
2023-12-15 13:13:47 +0800

    Add kprint_with_cls()

--- core/include/kernel_print.h
+++ core/include/kernel_print.h
@@ -1,3 +1,4 @@
 #pragma onces
 #include <ostype.h>
 uint_32 kprint(const char *fmt, ...);
+uint_32 kprint_with_cls(const char *fmt, ...);
--- core/kernel_print.c
+++ core/kernel_print.c
@@ -3,6 +3,17 @@
 #include <stdio.h>
 #include <print.h>
 
+uint_32 kprint_with_cls(const char *fmt, ...)
+{
+    cls_screen();
+    va_list args;
+    va_start(args, fmt);
+    char buf[1024] = {0};
+    vsprintf(buf, fmt, args);
+    va_end(args);
+    put_str(buf);
+    return 0;
+}
 uint_32 kprint(const char *fmt, ...)
 {
     va_list args;
@@ -13,3 +24,4 @@ uint_32 kprint(const char *fmt, ...)
     put_str(buf);
     return 0;
 }
+
