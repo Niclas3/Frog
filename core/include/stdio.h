678b3ff506a9f94cf1a65fbc90b5c26938910dc8
zm <missingurchin@gmail.com>
2023-12-15 14:46:03 +0800

    Refine printf() without buffer size limit

--- core/include/stdio.h
+++ core/include/stdio.h
@@ -6,6 +6,7 @@
 uint_32 vsprintf(char *str, const char *fmt, va_list ap);
 uint_32 sprintf(char* str, const char *fmt, ...);
 uint_32 printf(const char *fmt, ...);
+uint_32 lprintf(const char *fmt, ...);
 
 
 #endif
--- core/lib/stdio.c
+++ core/lib/stdio.c
@@ -4,6 +4,111 @@
 #include <string.h>
 #include <sys/syscall.h>
 
+/**
+ * The functions lvasprintf() are analogs of sprintf(3) and
+ * vsprintf(3),take a lambda() aka a function pointer,except that they allocate
+ *a string large enough to hold the output including the terminating null byte,
+ *and return a pointer to it via the first argument. This pointer should be
+ *passed to free(3) to release the allocated storage when it is no longer
+ *needed.
+ *****************************************************************************/
+
+typedef void (*print_fn)(void *str, uint_32 len);
+static uint_32 lvasprintf(void (*put_fn)(void *, uint_32),
+                          char *str,
+                          const char *fmt,
+                          va_list ap)
+{
+    char *s;
+    int d;
+    char c;
+    char *res = str;
+    int res_len = 0;
+    bool seen_mark = false;
+    while (*fmt) {
+        if (*fmt == '%' && !seen_mark) {
+            seen_mark = true;
+            fmt++;
+            continue;
+        }
+        if (seen_mark) {
+            switch (*fmt) {
+            case 's':
+                s = va_arg(ap, char *);
+                if (s == NULL) {
+                    s = "(null)";
+                }
+                int len = strlen(s);
+                put_fn(s, len);
+                res_len += len;
+                break;
+            case 'd':
+                // TODO: add feature
+                // /^\.[0-9]*d$/gm
+                d = va_arg(ap, int);
+                /* The number 2,147,483,647 (or hexadecimal 7FFFFFFF16) is the
+                 * maximum positive value for a 32-bit signed binary integer in
+                 * computing. */
+                // so i choose 10 to tmp string
+                char tmp[10] = {0};
+                int tlen = itoa(d, tmp, 10);
+                put_fn(tmp, tlen);
+                res_len += tlen;
+                break;
+            case 'x':
+                d = va_arg(ap, int);
+                char xtmp[10] = {0};
+                int xlen = itoa(d, xtmp, 16);
+                put_fn(xtmp, xlen);
+                res_len += xlen;
+                break;
+            case 'c':
+                c = (char) va_arg(ap, int);
+                put_fn(&c, 1);
+                res_len++;
+                break;
+            default:
+                put_fn(fmt, 1);
+                res_len++;
+                break;
+            }
+            seen_mark = false;
+        } else {
+            put_fn(fmt, 1);
+            res_len++;
+        }
+        fmt++;
+    }
+    return res_len;
+}
+
+static void print_to_stdout(void *str, uint_32 len)
+{
+    write(1, str, len);
+}
+
+uint_32 lvprintf(print_fn callback, const char *fmt, ...)
+{
+    va_list args;
+    uint_32 len;
+    char *str;
+    va_start(args, fmt);
+    lvasprintf(callback, str, fmt, args);
+    va_end(args);
+    return len;
+}
+
+uint_32 printf(const char *fmt, ...)
+{
+    va_list args;
+    uint_32 len;
+    char *str;
+    va_start(args, fmt);
+    lvasprintf(print_to_stdout, str, fmt, args);
+    va_end(args);
+    return len;
+}
+
 uint_32 vsprintf(char *str, const char *fmt, va_list ap)
 {
     char *s;
@@ -21,6 +126,9 @@ uint_32 vsprintf(char *str, const char *fmt, va_list ap)
             switch (*fmt) {
             case 's':
                 s = va_arg(ap, char *);
+                if (s == NULL) {
+                    s = "(null)";
+                }
                 int len = strlen(s);
                 strncpy(res, s, len);
                 res += len;
@@ -72,14 +180,4 @@ uint_32 sprintf(char *str, const char *fmt, ...)
     len = vsprintf(str, fmt, args);
     va_end(args);
     return len;
-}
-
-uint_32 printf(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    char buf[1024] = {0};
-    vsprintf(buf, fmt, args);
-    va_end(args);
-    return write(1, buf, strlen(buf));
 }
