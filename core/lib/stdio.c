14cf891623d47ca50c10434cac9087aec0eb46dc
zm <missingurchin@gmail.com>
2023-12-15 15:56:18 +0800

    Refine stdio.c prinf* function cluster

--- core/lib/stdio.c
+++ core/lib/stdio.c
@@ -12,9 +12,22 @@
  *passed to free(3) to release the allocated storage when it is no longer
  *needed.
  *****************************************************************************/
+struct STR_DATA {
+    char *str;
+    uint_32 len;
+    uint_32 written;
+};
 
-typedef void (*print_fn)(void *str, uint_32 len);
-static uint_32 lvasprintf(void (*put_fn)(void *, uint_32),
+typedef void (*print_fn)(void *userData, char);
+
+#define PUT(c)                 \
+    do {                       \
+        put_fn(userData, (c)); \
+        written++;             \
+    } while (0)
+
+static uint_32 lvasprintf(void (*put_fn)(void *, char),
+                          void *userData,
                           char *str,
                           const char *fmt,
                           va_list ap)
@@ -24,6 +37,7 @@ static uint_32 lvasprintf(void (*put_fn)(void *, uint_32),
     char c;
     char *res = str;
     int res_len = 0;
+    uint_32 written = 0;
     bool seen_mark = false;
     while (*fmt) {
         if (*fmt == '%' && !seen_mark) {
@@ -39,7 +53,10 @@ static uint_32 lvasprintf(void (*put_fn)(void *, uint_32),
                     s = "(null)";
                 }
                 int len = strlen(s);
-                put_fn(s, len);
+                while (*s) {
+                    PUT(*s);
+                    s++;
+                }
                 res_len += len;
                 break;
             case 'd':
@@ -51,30 +68,41 @@ static uint_32 lvasprintf(void (*put_fn)(void *, uint_32),
                  * computing. */
                 // so i choose 10 to tmp string
                 char tmp[10] = {0};
+                char *p_tmp = tmp;
                 int tlen = itoa(d, tmp, 10);
-                put_fn(tmp, tlen);
+                while (*p_tmp) {
+                    PUT(*p_tmp);
+                    p_tmp++;
+                }
                 res_len += tlen;
                 break;
             case 'x':
                 d = va_arg(ap, int);
                 char xtmp[10] = {0};
+                char *p_xtmp = xtmp;
                 int xlen = itoa(d, xtmp, 16);
-                put_fn(xtmp, xlen);
+                while (*p_xtmp) {
+                    PUT(*p_xtmp);
+                    p_xtmp++;
+                }
                 res_len += xlen;
                 break;
             case 'c':
                 c = (char) va_arg(ap, int);
-                put_fn(&c, 1);
+                PUT(c);
                 res_len++;
                 break;
-            default:
-                put_fn(fmt, 1);
+            default: {
+                char fmt_c = (char) *fmt;
+                PUT(fmt_c);
                 res_len++;
                 break;
             }
+            }
             seen_mark = false;
         } else {
-            put_fn(fmt, 1);
+            char fmt_c = (char) *fmt;
+            PUT(fmt_c);
             res_len++;
         }
         fmt++;
@@ -82,102 +110,77 @@ static uint_32 lvasprintf(void (*put_fn)(void *, uint_32),
     return res_len;
 }
 
-static void print_to_stdout(void *str, uint_32 len)
+static void print_to_stdout(void *data, char c)
 {
-    write(1, str, len);
+    char buf[1] = {c};
+    write(1, buf, 1);
 }
 
-uint_32 lvprintf(print_fn callback, const char *fmt, ...)
+static void print_to_buf(void *data, char c)
+{
+    struct STR_DATA *user_data = data;
+    if (user_data->written < user_data->len) {
+        uint_32 idx = user_data->written;
+        user_data->str[idx] = c;
+        user_data->written++;
+    } else {
+    }
+}
+
+static uint_32 lprintf(print_fn callback, const char *fmt, ...)
 {
     va_list args;
-    uint_32 len;
+    uint_32 len = 0;
     char *str;
     va_start(args, fmt);
-    lvasprintf(callback, str, fmt, args);
+    len = lvasprintf(callback, NULL, str, fmt, args);
     va_end(args);
     return len;
 }
 
-uint_32 printf(const char *fmt, ...)
+static uint_32 lsprintf(print_fn callback, char *str, const char *fmt, ...)
 {
     va_list args;
-    uint_32 len;
-    char *str;
+    uint_32 len = 0;
+    struct STR_DATA user_data = {
+        .str = str,
+        .len = 1024
+    };
     va_start(args, fmt);
-    lvasprintf(print_to_stdout, str, fmt, args);
+    len = lvasprintf(callback, &user_data, str, fmt, args);
+    va_end(args);
+    return len;
+}
+
+uint_32 sprintf(char *str, const char *fmt, ...)
+{
+    va_list args;
+    uint_32 len = 0;
+    va_start(args, fmt);
+    len = lsprintf(print_to_buf, str, fmt, args);
     va_end(args);
     return len;
 }
 
 uint_32 vsprintf(char *str, const char *fmt, va_list ap)
 {
-    char *s;
-    int d;
-    char c;
-    char *res = str;
-    bool seen_mark = false;
-    while (*fmt) {
-        if (*fmt == '%' && !seen_mark) {
-            seen_mark = true;
-            fmt++;
-            continue;
-        }
-        if (seen_mark) {
-            switch (*fmt) {
-            case 's':
-                s = va_arg(ap, char *);
-                if (s == NULL) {
-                    s = "(null)";
-                }
-                int len = strlen(s);
-                strncpy(res, s, len);
-                res += len;
-                break;
-            case 'd':
-                // TODO: add feature
-                // /^\.[0-9]*d$/gm
-                d = va_arg(ap, int);
-                /* The number 2,147,483,647 (or hexadecimal 7FFFFFFF16) is the
-                 * maximum positive value for a 32-bit signed binary integer in
-                 * computing. */
-                // so i choose 10 to tmp string
-                char tmp[10] = {0};
-                int tlen = itoa(d, tmp, 10);
-                strncpy(res, tmp, tlen);
-                res += tlen;
-                break;
-            case 'x':
-                d = va_arg(ap, int);
-                char xtmp[10] = {0};
-                int xlen = itoa(d, xtmp, 16);
-                strncpy(res, xtmp, xlen);
-                res += xlen;
-                break;
-            case 'c':
-                c = (char) va_arg(ap, int);
-                *res++ = c;
-                break;
-            default:
-                *res++ = *fmt;
-                break;
-            }
-            seen_mark = false;
-        } else {
-            *res++ = *fmt;
-        }
-        fmt++;
-    }
-    *res = '\0';
-    int res_len = strlen(str);
-    return res_len;
+    uint_32 len = 0;
+    struct STR_DATA user_data = {
+        .str = str,
+        .len = 1024
+    };
+    len = lvasprintf(print_to_buf, &user_data, str, fmt, ap);
+    return len;
 }
 
-uint_32 sprintf(char *str, const char *fmt, ...)
+
+uint_32 printf(const char *fmt, ...)
 {
     va_list args;
-    uint_32 len;
+    uint_32 len = 0;
+    char *str;
     va_start(args, fmt);
-    len = vsprintf(str, fmt, args);
+    len = lvasprintf(print_to_stdout, NULL, str, fmt, args);
     va_end(args);
     return len;
 }
