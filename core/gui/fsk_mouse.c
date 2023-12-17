034fe81a4118f2f5f7304ebe2b82095af26007d5
zm <missingurchin@gmail.com>
2023-12-17 20:15:41 +0800

    Refine fsk_mouse.c
    1. add struct fsk_mouse

--- core/gui/fsk_mouse.c
+++ core/gui/fsk_mouse.c
@@ -2,13 +2,15 @@
 #include <hid/mouse.h>
 #include <ioqueue.h>
 #include <ostype.h>
-#include <sys/2d_graphics.h>
 
 #include <fifo.h>
 #include <string.h>
 #include <sys/memory.h>
 #include <sys/sched.h>
 #include <sys/threads.h>
+
+//for test
+#include <stdio.h>
 
 extern CircleQueue mouse_queue;
 
@@ -25,22 +27,23 @@ static void read_packet_from_queue(CircleQueue *queue,
     }
 }
 
-static void mouse_event_handler(Point *pot)
+static void mouse_event_handler(struct fsk_mouse *mouse)
 {
     uint_32 testx = 0;
     uint_32 testy = 0;
 
-    uint_32 cursor_x = pot->X;
-    uint_32 cursor_y = pot->Y;
-    sys_free(pot);
+    uint_32 cursor_x = mouse->point.X;
+    uint_32 cursor_y = mouse->point.Y;
+    gfx_context_t *ctx = mouse->ctx;
+    sys_free(mouse);
 
     FIFO *timer_queue = {0};
     char *buf = sys_malloc(2);
 
-    uint_32 org_color = fetch_color(cursor_x, cursor_y);
-    uint_32 rcolor = convert_argb(FSK_LIME_GREEN);
-    uint_32 lcolor = convert_argb(FSK_ORANGE_RED);
-    uint_32 dccolor = convert_argb(FSK_GOLD);
+    uint_32 org_color = fetch_color(ctx, cursor_x, cursor_y);
+    uint_32 rcolor = FSK_LIME_GREEN;
+    uint_32 lcolor = FSK_ORANGE_RED;
+    uint_32 dccolor = FSK_GOLD;
     uint_32 *default_color = NULL;
     uint_8 double_left_click = 0;
     uint_32 double_click_delay;
@@ -53,17 +56,40 @@ static void mouse_event_handler(Point *pot)
             uint_32 delta_x = packet.x_difference;
             uint_32 delta_y = packet.y_difference;
 
-            // redraw prev pixel
-            draw_2d_gfx_cursor(cursor_x, cursor_y, &org_color);
+            if ((cursor_x > 1 && cursor_x < ctx->width) &&
+                (cursor_y > 0 && cursor_y < ctx->height)) {
+                // redraw prev pixel
+                draw_2d_gfx_cursor(ctx, cursor_x, cursor_y, &org_color);
+            }
 
-            // erase cursor
-            cursor_x += delta_x;
-            cursor_y += (-delta_y);
+            //testcode
+            /* uint_32 label_x, label_y= 0; */
+            /* char str[200] = {0}; */
+            /* sprintf(str,"cursor(x:%d, y:%d)",cursor_x, cursor_y); */
+            /* uint_32 label_w = strlen(str) * 8 + 20; */
+            /* uint_32 label_h = 16; */
+            /* draw_2d_gfx_label(ctx, label_x, label_y, label_w, label_h, */
+            /*                   FSK_ORANGE, FSK_MEDIUM_PURPLE, str); */
+            /*  */
+            /* uint_32 label1_x = 0; */
+            /* uint_32 label1_y = label_h + 20; */
+            /* char str1[200] = {0}; */
+            /* sprintf(str,"delta (x:%d, y:%d)",delta_x, delta_y); */
+            /* uint_32 label1_w = strlen(str) * 8 + 20; */
+            /* uint_32 label1_h = 16; */
+            /* draw_2d_gfx_label(ctx, label1_x, label1_y, label1_w, label1_h, */
+            /*                   FSK_ORANGE, FSK_MEDIUM_PURPLE, str); */
+            //endtest
 
-            org_color = fetch_color(cursor_x, cursor_y);
+            bool is_cursorx_at_saftrange =
+                (cursor_x + delta_x > 1 && cursor_x + delta_x < ctx->width);
+            bool is_cursory_at_saftrange =
+                (cursor_y + delta_y > 0 && cursor_y + delta_y < ctx->height);
 
-            if ((cursor_x > 1 && cursor_x < g_gfx_mode->x_resolution) &&
-                (cursor_y > 0 && cursor_y < g_gfx_mode->y_resolution)) {
+            if (is_cursorx_at_saftrange && is_cursory_at_saftrange) {
+                cursor_x += delta_x;
+                cursor_y += (-delta_y);
+                org_color = fetch_color(ctx, cursor_x, cursor_y);
                 if (packet.buttons == LEFT_CLICK) {
                     if (double_left_click == 1 && fifo_rest(timer_queue) == 0) {
                         fifo_get_data(timer_queue);
@@ -71,27 +97,27 @@ static void mouse_event_handler(Point *pot)
 
                         // testcode
                         // Double left click event here
-                        draw_2d_gfx_cursor(cursor_x, cursor_y, &dccolor);
-                        draw_2d_gfx_asc_char(8, cursor_x, cursor_y, dccolor,
-                                             0x03);
+                        draw_2d_gfx_cursor(ctx, cursor_x, cursor_y, &dccolor);
+                        draw_2d_gfx_asc_char(ctx, 8, cursor_x, cursor_y,
+                                             dccolor, 0x03);
                         // testend
                     } else if (hold_left_click) {
                         // testcode
                         // hold left click event here
-                        draw_2d_gfx_asc_char(8, cursor_x, cursor_y, dccolor,
-                                             0x04);
+                        draw_2d_gfx_asc_char(ctx, 8, cursor_x, cursor_y,
+                                             dccolor, 0x04);
                         // testend
                     } else {
                         double_left_click = 1;
-                        uint_32 delay = 3500;
+                        uint_32 delay = 3500;  // 3500 comes up by test
                         init_fifo(timer_queue, 2, buf);
                         set_timer(delay, timer_queue, '1');
                         hold_left_click = true;
 
                         // testcode
                         // Single left click event here
-                        draw_2d_gfx_cursor(cursor_x, cursor_y, &lcolor);
-                        draw_2d_gfx_asc_char(8, testx, 0, lcolor,
+                        draw_2d_gfx_cursor(ctx, cursor_x, cursor_y, &lcolor);
+                        draw_2d_gfx_asc_char(ctx, 8, testx, 0, lcolor,
                                              packet.buttons);
                         testx += 16;
                         // testend
@@ -101,24 +127,29 @@ static void mouse_event_handler(Point *pot)
                     hold_left_click = false;
                     // testcode
                     // mouse right single click event here
-                    draw_2d_gfx_cursor(cursor_x, cursor_y, &rcolor);
+                    draw_2d_gfx_cursor(ctx, cursor_x, cursor_y, &rcolor);
                     // testend
                 } else {  // moving with out button push
                     // testcode
                     // mouse move event here
-                    draw_2d_gfx_cursor(cursor_x, cursor_y, default_color);
+                    draw_2d_gfx_cursor(ctx, cursor_x, cursor_y, default_color);
                     // testend
                     hold_left_click = false;
                 }
+            } else if (is_cursorx_at_saftrange && !is_cursory_at_saftrange) {
+                draw_2d_gfx_cursor(ctx, cursor_x, cursor_y, default_color);
+            } else if (is_cursory_at_saftrange && !is_cursorx_at_saftrange) {
+                draw_2d_gfx_cursor(ctx, cursor_x, cursor_y, default_color);
             }
         }
     }
 }
 
-void create_fsk_mouse(uint_32 cursor_x, uint_32 cursor_y)
+void create_fsk_mouse(gfx_context_t *ctx, uint_32 cursor_x, uint_32 cursor_y)
 {
-    Point *base_point = sys_malloc(sizeof(base_point));
-    base_point->X = cursor_x;
-    base_point->Y = cursor_y;
-    thread_start("mouse_GUI", 40, mouse_event_handler, base_point);
+    struct fsk_mouse *mouse = sys_malloc(sizeof(struct fsk_mouse));
+    mouse->point.X = cursor_x;
+    mouse->point.Y = cursor_y;
+    mouse->ctx = ctx;
+    thread_start("mouse_GUI", 40, mouse_event_handler, mouse);
 }
--- core/include/gui/fsk_mouse.h
+++ core/include/gui/fsk_mouse.h
@@ -1,6 +1,13 @@
 #ifndef __GUI_FSK_MOUSE_H
 #define __GUI_FSK_MOUSE_H
 #include <ostype.h>
-void create_fsk_mouse(uint_32 cursor_x, uint_32 cursor_y);
+#include <sys/2d_graphics.h>
+
+struct fsk_mouse{
+    gfx_context_t *ctx;
+    Point point;
+};
+
+void create_fsk_mouse(gfx_context_t *ctx, uint_32 cursor_x, uint_32 cursor_y);
 
 #endif
