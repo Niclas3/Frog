67d6b3557f6d996385e14427bce52591c7fe2168
zm <missingurchin@gmail.com>
2024-01-16 17:51:38 +0800

    Add more spinlock things

--- core/include/sys/spinlock.h
+++ core/include/sys/spinlock.h
@@ -1,7 +1,17 @@
 #pragma once
+
 typedef volatile struct {
   volatile int latch[1];
-} spin_lock_t;
+} spinlock_t;
+
+#define SPIN_LOCK_UNLOCKED (spinlock_t) { 0 }
+
+#define TEST_BIT(bit, array) (array[bit / 8] & (1 << (bit % 8)))
+
+#define spin_is_locked(lock)    (TEST_BIT(0x0, (lock).latch))
+#define spin_trylock(lock)      (!__sync_lock_test_and_set((lock).latch, 0x00))
+
+#define spin_unlock_wait(x)	do { } while (0)
 
 #define spin_init(lock)                                                        \
   do {                                                                         \
