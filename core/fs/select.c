4e1caba7e88d50e9ec88de32fe681e4d8ae246b0
zm <missingurchin@gmail.com>
2024-01-17 22:11:18 +0800

    Add test code at select.c

--- core/fs/select.c
+++ core/fs/select.c
@@ -1,8 +1,18 @@
 #include <fs/select.h>
 #include <fs/file.h>
+#include <sys/poll.h>
+#include <sys/threads.h>
 
+extern void remove_wait_queue(wait_queue_head_t *q, wait_queue_t * wait);
+extern void add_wait_queue(wait_queue_head_t *q, wait_queue_t * wait);
 uint_32 sys_wait2(int n, int_32 *fds, struct timeval *tvp){
-    struct file *file;
+    TCB_t *cur = running_thread();
+    DECLARE_WAITQUEUE(entry1, cur);
+    DECLARE_WAIT_QUEUE_HEAD(wait_queue_head);
 
+    init_waitqueue_head(&wait_queue_head);
+    init_waitqueue_entry(&entry1, cur);
+    add_wait_queue(&wait_queue_head, &entry1);
+    remove_wait_queue(&wait_queue_head, &entry1);
     return 0;
 }
