527eabe2382ea9559efc38d2185bed24df32a31e
zm <missingurchin@gmail.com>
2024-01-18 16:00:00 +0800

    Add new function pcmouse_create() to create pcmouse char device

--- core/device/pc_mouse.c
+++ core/device/pc_mouse.c
@@ -1,14 +1,26 @@
 #include <device/pc_mouse.h>
 #include <errno-base.h>
 #include <string.h>
+#include <math.h>
+
 #include <sys/fork.h>
 #include <sys/int.h>
 #include <sys/memory.h>
 #include <sys/threads.h>
-#include <fs/file.h>
+
+#include <device/ide.h>
+#include <device/devno-base.h>
+
 #include <fs/fcntl.h>
+#include <fs/dir.h>
+#include <fs/file.h>
+#include <fs/fs.h>
+#include <fs/inode.h>
 
 #include <debug.h>
+
+extern struct file g_file_table[MAX_FILE_OPEN];
+extern struct lock g_ft_lock;
 
 struct pc_mouse_pkg_queue {
     unsigned long head;
@@ -97,11 +109,10 @@ static void make_mouse_packet(struct mouse_raw_data *mdata)
     memcpy(&queue->buf[head], &packet, packet_size);
     head = (head + packet_size) & (MOUSE_PKG_BUF_SIZE - 1);
 
-    if (head != queue->tail) { // queue is not empty
+    if (head != queue->tail) {  // queue is not empty
         queue->head = head;
         wake_up_interruptible(&queue->proc_list);
     }
-
 }
 
 void handle_ps2_mouse_scancode(uint_8 scancode)
@@ -132,6 +143,111 @@ void handle_ps2_mouse_scancode(uint_8 scancode)
         make_mouse_packet(&mdata);
         break;
     }
+}
+
+/**
+ * create a char type file
+ *
+ * @return reture a global file table index
+ *****************************************************************************/
+int_32 pcmouse_create(struct partition *part,
+                      struct dir *parent_d,
+                      char *name,
+                      void *target)
+{
+    uint_8 rollback_step = 0;
+    char *buf = sys_malloc(1024);
+    if (!buf) {
+        //  kprint("Not enough memory for io buf");
+        return -1;
+    }
+
+    // 1. Need new inode aka create a inode (inode_open())
+    uint_32 inode_nr = inode_bitmap_alloc(part);
+    if (inode_nr == -1) {
+        // TODO:
+        //  kprint("Not enough inode bitmap position.");
+        return -1;
+    }
+    ASSERT(inode_nr != -1);
+    struct inode *new_f_inode = sys_malloc(sizeof(struct inode));
+    if (!new_f_inode) {
+        // TODO:
+        //  kprint("Not enough memory for inode .");
+        // Recover! Need recover inode bitmap set
+        rollback_step = 1;
+        goto roll_back;
+    }
+    new_inode(inode_nr, new_f_inode);
+
+    new_f_inode->i_mode = FT_CHAR << 11;
+    new_f_inode->i_zones[0] = (uint_32 *) target;
+    new_f_inode->i_count++;
+    new_f_inode->i_dev = DNOPCMOUSE;
+    // 2. new dir_entry
+    struct dir_entry new_entry;
+    new_dir_entry(name, inode_nr, FT_CHAR, &new_entry);
+    // 3.get file slot form global file_table
+    lock_fetch(&g_ft_lock);
+    // global file table index
+    uint_32 fd_idx = occupy_file_table_slot();
+    if (fd_idx == -1) {
+        // TODO:
+        //  kprint("Not enough slot at file table.");
+        // Recover! Need recover inode bitmap set
+        //        ! free new_f_inode
+        rollback_step = 2;
+        goto roll_back;
+    }
+
+    g_file_table[fd_idx].fd_pos = 0;
+    g_file_table[fd_idx].fd_inode = new_f_inode;
+    g_file_table[fd_idx].fd_inode->i_lock = false;
+    lock_release(&g_ft_lock);
+
+    // 4. flush dir_entry to parents directory
+    // search new entry first
+    int_32 failed = search_dir_entry(part, name, parent_d, &new_entry);
+    if (!failed) {
+        list_add(&new_f_inode->inode_tag, &part->open_inodes);
+        sys_free(buf);
+        return fd_idx;
+    } else {
+        if (flush_dir_entry(part, parent_d, &new_entry, buf)) {
+            // TODO:
+            //  kprint("Failed at flush directory entry");
+            rollback_step = 3;
+            // Recover! Need recover inode bitmap set
+            //        ! free new_f_inode
+            //        ! clear g_file_table[fd_idx]
+            goto roll_back;
+        }
+    }
+
+    // 5. flush parent inode
+    //  flush_dir_entry will change parent directory inode size
+    /* memset(buf, 0, 1024);  // 1024 for 2 sectors */
+    /* flush_inode(part, parent_d->inode, buf); */
+    /* 6. flush new inode */
+    /* memset(buf, 0, 1024);  // 1024 for 2 sectors */
+    /* flush_inode(part, new_f_inode, buf); */
+
+    // 7. flush inode bitmap and zones bitmap
+    /* flush_bitmap(part, INODE_BITMAP, inode_nr); */
+
+
+roll_back:
+    switch (rollback_step) {
+    case 3:
+        memset(&g_file_table[fd_idx], 0, sizeof(struct file));
+    case 2:
+        sys_free(new_f_inode);
+    case 1:
+        set_value_bitmap(&part->inode_bitmap, inode_nr, 0);
+        break;
+    }
+    sys_free(buf);
+    return -1;
 }
 
 uint_32 read_pcmouse(struct file *file, void *buf, uint_32 count)
--- core/include/device/pc_mouse.h
+++ core/include/device/pc_mouse.h
@@ -12,9 +12,16 @@
 #define MOUSE_PKG_BUF_SIZE sizeof(mouse_device_packet_t) * 1024
 
 struct file;
+struct partition;
+struct dir;
 
 void handle_ps2_mouse_scancode(uint_8 scancode);
 uint_32 pc_mouse_init(void);
+
+int_32 pcmouse_create(struct partition *part,
+                      struct dir *parent_d,
+                      char *name,
+                      void *target);
 uint_32 read_pcmouse(struct file *file, void *buf, uint_32 count);
 
 
