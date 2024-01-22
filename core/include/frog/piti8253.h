de52a51b041130f01a9655697fa1ad731f93cf74
zm <missingurchin@gmail.com>
2024-01-22 16:55:44 +0800

    Remove IRQ0_FREQUENCY from piti8253.h

--- core/include/frog/piti8253.h
+++ core/include/frog/piti8253.h
@@ -2,10 +2,6 @@
 #define __FROG_PIT8253_H
 #include <ostype.h>
 
-// #define IRQ0_FREQUENCY   100
-// #define IRQ0_FREQUENCY   1000
-#define IRQ0_FREQUENCY   9000
-// #define IRQ0_FREQUENCY   12000
 
 // init PIT (programmable interval timer)
 void init_PIT8253(void);
