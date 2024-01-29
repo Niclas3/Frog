30a5465875185ef5fb03ea0c834e232b1b470f89
zm <missingurchin@gmail.com>
2024-01-29 10:01:50 +0800

    Add struct poudland_context and poudland_window
    1. poudland_context for context
    2. poudland_window for client window

--- core/include/gua/poudland.h
+++ core/include/gua/poudland.h
@@ -1,4 +1,66 @@
 #pragma once
 #include <ostype.h>
+#include <list.h>
+#include <gua/2d_graphics.h>
 typedef uint_32 poudland_wid_t;
-void poudland_init(void);
+
+/*
+ * Server connection context.
+ */
+typedef struct poudland_context {
+	void * sock;
+
+	/* server display size */
+	uint_32 display_width;
+	uint_32 display_height;
+
+	/* Hash of window IDs to window objects */
+	// hashmap_t * windows;
+
+	/* queued events */
+	struct list_head equeue;
+
+	/* server identifier string */
+	char * server_ident;
+} poudland_t;
+
+typedef struct poudland_window {
+	/* Server window identifier, unique to each window */
+	poudland_wid_t wid;
+
+	/* Window size */
+	uint_32 width;
+	uint_32 height;
+
+	/* Window backing buffer */
+	char * buffer;
+	/*
+	 * Because the buffer can change during resizing,
+	 * buffers are indexed to ensure we are using
+	 * the one the server expects.
+	 */
+	uint_32 bufid;
+
+	/* Window focused flag */
+	uint_8 focused;
+
+	/* Old buffer ID */
+	uint_32 oldbufid;
+
+	/* Generic pointer for client use */
+	void * user_data;
+
+	/* Window position in the server; automatically updated */
+	int_32 x;
+	int_32 y;
+
+	/* Flags for the decorator library to use */
+	uint_32 decorator_flags;
+
+	/* Server context that owns this window */
+	poudland_t * ctx;
+
+	int_32 mouse_state;
+} poudland_window_t;
+
+gfx_context_t *init_graphics_poudland_double_buffer(poudland_window_t *window);
--- core/lib/poudland.c
+++ core/lib/poudland.c
@@ -1,15 +1,34 @@
 // Window compositor
 
 // watch mouse and keyboard event
-#include <hid/mouse.h>
-#include <ioqueue.h>
-#include <ostype.h>
 #include <gua/poudland.h>
+#include <hid/mouse.h>
+#include <ostype.h>
 #include <stdio.h>
 #include <string.h>
 #include <syscall.h>
 
-void poudland_init(void)
+/**
+ * Create a graphical context around a poudland window.
+ */
+gfx_context_t *init_graphics_poudland(poudland_window_t *window)
 {
-
+    gfx_context_t *out = malloc(sizeof(gfx_context_t));
+    out->width = window->width;
+    out->height = window->height;
+    out->stride = window->width * sizeof(uint_32);
+    out->depth = 32;
+    out->size = GFX_H(out) * GFX_W(out) * GFX_D(out);
+    out->buffer = window->buffer;
+    out->backbuffer = out->buffer;
+    out->clips = NULL;
+    return out;
 }
+
+gfx_context_t *init_graphics_poudland_double_buffer(poudland_window_t *window)
+{
+    gfx_context_t *out = init_graphics_poudland(window);
+    out->backbuffer = malloc(GFX_D(out) * GFX_W(out) * GFX_H(out));
+    return out;
+}
+
