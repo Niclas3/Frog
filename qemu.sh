f3b72d3dbc55a60327283ae51eecf61cbf6d85ad
zm <missingurchin@gmail.com>
2024-06-04 12:07:37 +0800

    fix(lfb): quick fix lfb not clean when close os

--- qemu.sh
+++ qemu.sh
@@ -1,7 +1,6 @@
-# pkill bochs &&
 make clean-all && 
 make newimg && 
+make newhd80img && #FIX me free lfb <-- this is a bug 
 make mount_debug && 
 make run &
-# ~/bin/bin/bochs -q -f ./bochsrc.debug & 
 gdb -x qemu_debug_script.gdb
