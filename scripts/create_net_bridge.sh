45ef631568e170282bd4564373000c6524578da2
zm <missingurchin@gmail.com>
2024-12-16 14:13:06 +0800

    chore(scripts): create a net bridge and a tap interface

---
+++ scripts/create_net_bridge.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# test if this pc has a network bridge named br0 and has a tap name tap0 then 
+# use it. if not create. You should ask for outstream NIC name.
+
+NIC="enp0s31f6"
+
+brctl addbr br0
+tunctl -t tap0 -u `whoami`
+
+brctl addif br0 tap0 $(NIC)
+
+ip link set up br0
+ip link set up tap0
+
+ip link set promisc dev br0
+ip link set promisc dev tap0
+
