6fd616f6979228ff36c5bd91893960038fe042c9
zm <missingurchin@gmail.com>
2025-04-26 01:24:49 +0800

    refine(softirq): use cpu at softirq code
    WHY:
    - I import cpu.c into Frog, now use it at softirq code
    
    WHAT:
    - change all global variables to local_cpu attributes
    
    HOW TESTED:
    - tested
    
    tags: [softirq, local_cpu]

--- core/kernel/softirq.c
+++ core/kernel/softirq.c
@@ -1,57 +1,58 @@
 #include <frog/interrupt.h>
-#include <frog/softirq.h>
 #include <frog/irqflags.h>
+#include <frog/softirq.h>
+#include <kernel/cpu.h>
+#include <kernel/debug.h>
 
-//`g_softirq_pending` this global variable is for marking different types
-// softirqs
-uint_32 g_softirq_pending = 0x0;
-
-// actual softirq action handler
-// register by register_softirq()
-// this is a global array
-struct softirq_action g_softirq_handlers[NR_SOFTIRQ];
 
 // clear typical softirq type marked bits
 void clear_softirq(uint_32 type)
 {
-        g_softirq_pending &= ~(1UL << type);
+        this_cpu()->softirq_pending &= ~(1UL << type);
 }
 
 void do_softirq(void)
 {
-        struct softirq_action *h = g_softirq_handlers;
+        struct softirq_action *h = this_cpu()->softirq_handlers;
+        uint_32 type = 0;
         do {
-                uint_32 type = 0;
-                if (g_softirq_pending & (1 << type)) {
+                if (this_cpu()->softirq_pending & (1 << type)) {
                         clear_softirq(type);
                         h->action(h);
                 }
                 type++;
                 h++;
-        } while (g_softirq_pending);
+        } while (this_cpu()->softirq_pending);
 }
 
 void register_softirq(uint_32 type, void (*handler)(struct softirq_action *))
 {
-        g_softirq_handlers[type].action = handler;
+        this_cpu()->softirq_handlers[type].action = handler;
 }
 
 // raise a softirq at a ISR when it should be mark as panding.
 void raise_softirq(uint_32 type)
 {
-        g_softirq_pending |= (1 << type);
+        this_cpu()->softirq_pending |= (1 << type);
 }
 
-// Exit IRQ from this function
+// Enter IRQ (use interrupt stack) from this function
+void irq_enter(void)
+{
+        /* struct cpu_local *cur_cpu = this_cpu(); */
+
+        //!! enter interrupt stack CPU per stack
+}
+
+// Exit IRQ (from interrupt stack to kernel stack) from this function
 void irq_exit(void)
 {
         // test if there is no softirq panding
-        if (g_softirq_pending) {
-                local_irq_enable();
+        if (this_cpu()->softirq_pending) {
+                /* local_irq_enable(); */
                 do_softirq();
                 local_irq_disable();
         }
-        // test TIF_NEED_RESCHED
 }
 
 
