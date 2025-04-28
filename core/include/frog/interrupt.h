02ba3a20f7c030ba0016647bc1d310d058f07a1c
zm <missingurchin@gmail.com>
2025-04-28 14:49:15 +0800

    refactor(interrupt): move softirq api from frog/interrupt.h to softirq.h
    WHY:
    - I don't want to include all frog/interrput.h when I only want to use
      softirq.h
    
    WHAT:
    - move softirq api to softirq.h, nothing involved.
    
    HOW TESTED:
    - tested
    
    tags: [interrupt, softirq]

--- core/include/frog/interrupt.h
+++ core/include/frog/interrupt.h
@@ -2,48 +2,7 @@
 #define _FROG_INTERRUPT_H
 #include <frog/types.h>
 #include <asm/int.h>
-
-// all softirq types
-enum {
-        HI_SOFTIRQ = 0,
-        TIMER_SOFTIRQ,
-        NET_TX_SOFTIRQ,
-        NET_RX_SOFTIRQ,
-        BLOCK_SOFTIRQ,
-        HRTIMER_SOFTIRQ,
-
-        NR_SOFTIRQ
-};
-
-
-struct softirq_action {
-        void (*action)(struct softirq_action *);
-};
-extern void softirq_init(void);
-
-// Send EOI to interrupt controller 
-// @intno number of int vector what interrupt call it
-extern void ack(uint_32 intno);
-
-// exit ISR
-extern void irq_exit(void);
-
-//Softirq
-// this function to pick a softirq action and invoke it.
-extern void do_softirq(void);
-
-// register a function when a type of softirq raised invoke that function by
-// do_softirq()
-
-extern void register_softirq(uint_32 type, void (*handler)(struct softirq_action *));
-
-// raise a softirq at a ISR when it should be mark as panding.
-extern void raise_softirq(uint_32 type);
-
-// clear typical softirq type marked bits
-extern void clear_softirq(uint_32 type);
-
-
+#include <frog/softirq.h>
 
 extern void register_intr_handler(uint_32 int_vector_code, Inthandle_t handler);
 
--- core/include/frog/softirq.h
+++ core/include/frog/softirq.h
@@ -2,4 +2,47 @@
 #define __SYS_SOFTIRQ_H
 #include <frog/interrupt.h>
 
+// all softirq types
+enum {
+        HI_SOFTIRQ = 0,
+        TIMER_SOFTIRQ,
+        NET_TX_SOFTIRQ,
+        NET_RX_SOFTIRQ,
+        BLOCK_SOFTIRQ,
+        HRTIMER_SOFTIRQ,
+
+        NR_SOFTIRQ
+};
+
+#define MAX_SOFTIRQ_LOOP 8
+
+struct softirq_action {
+        void (*action)(struct softirq_action *);
+};
+extern void softirq_init(void);
+
+// Send EOI to interrupt controller 
+// @intno number of int vector what interrupt call it
+extern void ack(uint_32 intno);
+
+// enter interrupt stack and some irq hooks
+extern void irq_enter(void);
+// exit ISR
+extern void irq_exit(void);
+
+//Softirq
+// this function to pick a softirq action and invoke it.
+extern void do_softirq(void);
+
+// register a function when a type of softirq raised invoke that function by
+// do_softirq()
+
+extern void register_softirq(uint_32 type, void (*handler)(struct softirq_action *));
+
+// raise a softirq at a ISR when it should be mark as panding.
+extern void raise_softirq(uint_32 type);
+
+// clear typical softirq type marked bits
+extern void clear_softirq(uint_32 type);
+
 #endif
