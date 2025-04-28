269d84c8c34e642ab09a6f3da7ce22b8cb21a7cb
zm <missingurchin@gmail.com>
2025-04-28 15:01:08 +0800

    feat(softirq): use `MAX_SOFTIRQ_LOOP` preventing softirq starvation
    WHY:
    - If an interrupt keeps comming in and interrups a running softirq, the
      softirq may starve. and it called `interrput storm`.
    - Frog currently does not have `ksoftirqd`, So now I printk a warning
      when softirq stavation is detected.
    
    WHAT:
    - When softirq loop reaches  `MAX_SOFTIRQ_LOOP`, it will break out of
      the `do_sofirq` loop.
    
    HOW TESTED:
    - Maybe `ksoftirqd` is needed to properly resolve this situation
      in future.
    
    tags: [softirq]

--- core/kernel/softirq.c
+++ core/kernel/softirq.c
@@ -3,6 +3,8 @@
 #include <frog/softirq.h>
 #include <kernel/cpu.h>
 #include <kernel/debug.h>
+#include <asm/interrupt.h>
+
 
 
 // clear typical softirq type marked bits
@@ -15,13 +17,18 @@ void do_softirq(void)
 {
         struct softirq_action *h = this_cpu()->softirq_handlers;
         uint_32 type = 0;
+        uint_32 loop = 0;
         do {
                 if (this_cpu()->softirq_pending & (1 << type)) {
                         clear_softirq(type);
                         h->action(h);
                 }
-                type++;
-                h++;
+                type++; // maybe here will be a bug. type and h will overflow
+                h++;    // if you take care of softirq_pending that fine.
+                if(loop >= MAX_SOFTIRQ_LOOP){
+                        WARN("do_softirq: possible softirq storm, breaking after %d loops \n", loop);
+                        break;
+                }
         } while (this_cpu()->softirq_pending);
 }
 
@@ -39,7 +46,8 @@ void raise_softirq(uint_32 type)
 // Enter IRQ (use interrupt stack) from this function
 void irq_enter(void)
 {
-        /* struct cpu_local *cur_cpu = this_cpu(); */
+        struct cpu_local *cur_cpu = this_cpu();
+        INFO("interrupt stack top: %x\n", cur_cpu->irq_stack_top);
 
         //!! enter interrupt stack CPU per stack
 }
@@ -47,9 +55,10 @@ void irq_enter(void)
 // Exit IRQ (from interrupt stack to kernel stack) from this function
 void irq_exit(void)
 {
+
         // test if there is no softirq panding
         if (this_cpu()->softirq_pending) {
-                /* local_irq_enable(); */
+                local_irq_enable();
                 do_softirq();
                 local_irq_disable();
         }
