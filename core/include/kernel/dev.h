c1ccbec7cef84bbcbde8dc4d7998c89b274211c5
zm <missingurchin@gmail.com>
2025-05-20 17:43:49 +0800

    feat(vfs): dev.h api and mount.h api
    WHY:
    - just mount.h and dev.h API
    
    WHAT:
    
    HOW TESTED:
    - not tested
    
    tags: [vfs]

---
+++ core/include/kernel/dev.h
@@ -0,0 +1,6 @@
+#ifndef __FROG_KERNEL_DEV_H
+#define __FROG_KERNEL_DEV_H
+
+//
+
+#endif
---
+++ core/include/kernel/mount.h
@@ -0,0 +1,29 @@
+#ifndef __FROG_KERNEL_MOUNT_H
+#define __FROG_KERNEL_MOUNT_H
+
+#include <frog/list.h>
+struct fs_type {
+        const char *name;
+        struct super_block *(*mount)(struct fs_type *fs,
+                                     int flags,
+                                     const char *dev,
+                                     void *data);
+        struct list_head fs_type_node;
+};
+
+struct mount_entry {
+        struct dentry *mount_point;
+        struct super_block *sb;
+        struct list_head mount_node;
+};
+
+void init_mount_list(void);
+void init_fs_type_list(void);
+
+void add_mount_list(struct list_head *node);
+struct dentry *find_mounted_dentry(struct dentry *dir);
+int register_fs(struct fs_type *fs_type);
+struct fs_type *find_fs_type(const char *name);
+struct mount_entry *find_mount_entry(const char *name);
+
+#endif
