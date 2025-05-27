519041d2311a44506b3ba6ce6d79f6f4cb9f495b
zm <missingurchin@gmail.com>
2025-05-27 13:46:09 +0800

    feat(linker): support ld script
    WHY:
    - Use ld script can handle more specific memory details.
    - like start/end address of .bss segments
    - and you can arrange each segments in what address what you want
    - maybe later Frogos can add custom-linked symbol for driver or
    - something
    
    WHAT:
    - Those all linked symbols were introduced
    - char __rodata_start[];
    - char _end[];     // this mean the end of .bss also end of kernel code
    - char __data_start[];
    - char __data_end[];
    - char __bss_start[];
    
    HOW TESTED:
    - tested
    
    tags: [linker, ld, script]

---
+++ core/include/frog/linker.h
@@ -0,0 +1,11 @@
+#ifndef __FROG_LINKER_H
+#define __FROG_LINKER_H
+
+/// link time added section bounds symbols
+extern char __rodata_start[];
+extern char _end[];
+extern char __data_start[];
+extern char __data_end[];
+extern char __bss_start[];
+
+#endif
